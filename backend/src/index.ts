4c642abb0e8cac620f10847ba4bee1a9b44dfe41
伊藤尚宏 <itounaohiro@itounaohironoMacBook-Pro.local>
2025-06-15 20:00:53 +0900

    フロントエンドのメッセージを変更

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ app.use(cors()); // Vueと通信するためCORSを有効に
 app.use(express.json());
 
 app.get('/api/hello', (_req, res) => {
-  res.json({ message: 'ExpressとTypeScriptの世界へようこそ!' });
+  res.json({ message: 'TypescriptベースのVueとExpressの世界へようこそ! 今後はFirebase + GeminiAPI + VantageAPIを実装予定！' });
 });
 
 app.listen(port, () => {
